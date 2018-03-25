a56f8df4166938973d8bb52c6f895b73fba3d1b8
Oleg <o.selivancev@gmail.com>
2018-03-25 14:55:15 +0300

    telbook 2503 _2

--- Week2gh/tel.js
+++ Week2gh/tel.js
@@ -1,13 +1,10 @@
 
 var book = [];
 var namesMap = {};
+var telarray = [];
 
 function add(name, tel){
-
-//console.log('try add:', name,'',tel);
-
     if (book.length == 0 ) {
-        console.log('book clear');
         book.push({name: name, tels: tel});
         namesMap[name] = true;
     }
@@ -28,9 +25,42 @@ function add(name, tel){
                     }
                 }
     }
-console.log(namesMap);
 }
 
+
+function remove(tel) {
+    var telarray = [];
+var a = tel;
+
+    for (var i=0; i<book.length; i++ )
+    {
+        telarray = book[i].tels.split(',');
+        console.log('telarray', telarray);
+
+    //telarray.forEach(telFilter);
+    var telarrayfiltered = telarray.filter(telFilter);
+    console.log(telarrayfiltered);
+
+    function telFilter(telnumb, index) {
+        if (telarray == telnumb){
+            var tags = telarray;
+            return tags.indexOf(a) !== -1;
+        }
+    }
+
+     /*   for (j=0; j < telarray; j++)
+                {
+                    if (telarray[j] = tel)
+                    {
+                        console.log('j',j);
+                        telarrayfiltered.push(telarray[j]);
+                        console.log('telarrayfiltered=', telarrayfiltered);
+                    }
+                } */
+    book[i].tels = telarrayfiltered.join(',');
+    }
+
+};
 
 add('Valya','74957654321');
 add('Valya','32178840');
@@ -38,8 +68,11 @@ add('Ivan','74957654321');
 add('Valya','32178840,3321231231');
 add('Ivan','7444455');
 add('Vadim','45678955');
-console.log('result:', book);
+add('Vadim','4567843335');
+add('Vadim','45678957782325');
+//console.log('result:', book);
 
-
+remove('4567843335');
+console.log(book);
 
 
