21f2d32b30b5c02cdb5ce49f8a155b3cfd4ef672
Oleg <o.selivancev@gmail.com>
2018-03-27 16:17:51 +0300

    WORK!

--- Week2gh/index.js
+++ Week2gh/index.js
@@ -1,14 +1,82 @@
 // Телефонная книга
-var phoneBook = [];
+var book = [];
 var namesMap = {};
-var result = [];
 /**
  * @param {String} command
  * @returns {*} - результат зависит от команды
  */
 module.exports = function (command) {
 
+    function add(name, tel) {
+        if (book.length == 0) {
+            book.push({name: name, tels: tel});
+            namesMap[name] = true;
+        }
+        else
+        {
+            if (!namesMap.hasOwnProperty(name)) {
+                book.push({name: name, tels: tel});
+                namesMap[name] = true;
+            }
+            else
+            {
+                for (var i = 0; i < book.length; i++)
+                {
+                    if (book[i].name == name)
+                    {
+                        if (book[i].tels.length == 0) {book[i].tels = book[i].tels + tel}
+                        else {book[i].tels = book[i].tels + ',' + tel}
+                    }
+                }
+            }
+        }
+    }
+
+    function remove(tel) {
+        var removeresult = false;
+        var telarray = [];
+        for (var i = 0; i < book.length; i++ )
+        {
+            telarray = book[i].tels.split(',');
+            var telarrayfiltered = telarray.filter(function (telnum) {return telnum != tel});
+
+            book[i].tels = telarrayfiltered.join(',');
+            if (telarray.length != telarrayfiltered.length){removeresult = true}
+        }
+        return removeresult;
+    }
+
+    function show() {
+        var telsShowArray=[];
+        var result = [];
+        for (var i=0; i<book.length; i++)
+        {
+            if (book[i].tels !== '')
+            {
+                telsShowArray = book[i].tels.split(',')
+                result.push(book[i].name+':'+' '+telsShowArray.join(', '));
+            }
+        }
+        return result.sort();
+    }
 
 
+        var commandArray = command.split(' ');
 
-};
+        if (commandArray[0] == 'ADD')
+        {
+            add(commandArray[1],commandArray[2])
+        }
+
+        if (commandArray[0] == 'REMOVE_PHONE')
+        {
+            return remove(commandArray[1]);
+        }
+
+        if (commandArray[0] == 'SHOW')
+        {
+            return show();
+        }
+
+    };
+
--- Week2gh/tel.js
+++ Week2gh/tel.js
@@ -3,52 +3,59 @@ var book = [];
 var namesMap = {};
 
 
-function add(name, tel){
-    if (book.length == 0 ) {
+function add(name, tel) {
+    if (book.length == 0) {
         book.push({name: name, tels: tel});
         namesMap[name] = true;
     }
-    else {
-        if (!namesMap.hasOwnProperty(name))
-        {
-           book.push({name: name, tels: tel});
-           namesMap[name] = true;
+    else
+    {
+        if (!namesMap.hasOwnProperty(name)) {
+            book.push({name: name, tels: tel});
+            namesMap[name] = true;
         }
-            else
-                for (var i = 0; i < book.length; i++) {
-
-                    {
-                    if (book[i].name == name) {
-                        book[i].tels = book[i].tels + ',' + tel;}
-                    }
+        else
+        {
+            for (var i = 0; i < book.length; i++)
+            {
+                if (book[i].name == name)
+                {
+                    if (book[i].tels.length == 0) {book[i].tels = book[i].tels + tel}
+                    else {book[i].tels = book[i].tels + ',' + tel}
                 }
+            }
+        }
     }
 }
+
 
 function remove(tel) {
     var removeresult = false;
     var telarray = [];
-    for (var i = 0; i < book.length; i++ )
-    {
+    for (var i = 0; i < book.length; i++) {
         telarray = book[i].tels.split(',');
-        var telarrayfiltered = telarray.filter(function (telnum) {return telnum != tel});
-
+        var telarrayfiltered = telarray.filter(function (telnum) {
+            return telnum != tel
+        });
         book[i].tels = telarrayfiltered.join(',');
-        if (telarray.length != telarrayfiltered.length){removeresult = true}
+        if (telarray.length != telarrayfiltered.length) {removeresult = true}
+
+        return removeresult;
     }
-    return removeresult;
 }
 
 function show() {
+    var telsShowArray=[];
     var result = [];
     for (var i=0; i<book.length; i++)
     {
         if (book[i].tels !== '')
         {
-            result.push(book[i].name+':'+' '+book[i].tels);
+            telsShowArray = book[i].tels.split(',')
+            result.push(book[i].name+':'+' '+telsShowArray.join(', '));
         }
     }
-   return result.sort();
+    return result.sort();
 }
 
 function phoneBook(cmd) {
@@ -57,19 +64,18 @@ var commandArray = cmd.split(' ');
 
     if (commandArray[0] == 'ADD')
     {
-    console.log('CMD:',commandArray[0],' name:',commandArray[1],' tels:', commandArray[2]);
+    //console.log('CMD:',commandArray[0],' name:',commandArray[1],' tels:', commandArray[2]);
     add(commandArray[1],commandArray[2])
     }
 
     if (commandArray[0] == 'REMOVE_PHONE')
     {
-        console.log('CMD:',commandArray[0],' tel:', commandArray[1]);
-        console.log('remove result:', remove(commandArray[1]));
+        return remove(commandArray[1]);
     }
 
     if (commandArray[0] == 'SHOW')
     {
-        console.log(show());
+        return show();
     }
 
 }
@@ -85,31 +91,25 @@ console.log(remove('555-20-01'));
 console.log(show());
 */
 
-
+/*
 phoneBook('ADD Ivan 555-10-01,555-10-03');
-
 phoneBook('ADD Ivan 555-10-02');
-
-phoneBook('SHOW');
-
-// Вывод:
-
-// ["Ivan: 555-10-01, 555-10-03, 555-10-02"]
-
-phoneBook('REMOVE_PHONE 555-10-03');
-
+console.log(phoneBook('SHOW'));
+console.log(phoneBook('REMOVE_PHONE 555-10-03'));
 phoneBook('ADD Alex 555-20-01');
-
-phoneBook('SHOW');
-
-// Вывод:
-
-// ["Alex: 555-20-01", "Ivan: 555-10-01, 555-10-02"]
-
+console.log(phoneBook('SHOW'));
 phoneBook('REMOVE_PHONE 555-20-01');
+console.log(phoneBook('SHOW'));
+*/
 
-phoneBook('SHOW');
 
-// Вывод:
 
-// ["Ivan: 555-10-01, 555-10-02"]
+phoneBook('ADD Ivan 555,666')
+phoneBook('ADD Alex 777')
+phoneBook('ADD Alex 333')
+phoneBook('REMOVE_PHONE 555')
+phoneBook('REMOVE_PHONE 666')
+phoneBook('ADD Ivan 888')
+console.log(phoneBook('SHOW'));
+
+
