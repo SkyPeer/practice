634d52273cc70b4364dca4d1f87d4846ef10d9b8
Oleg <o.selivancev@gmail.com>
2018-05-22 12:35:10 +0300

    Event exmple update
    22/05/2018

--- Week5gh/event.js
+++ Week5gh/event.js
@@ -17,38 +17,42 @@ var PubSub = function ()
 
     var handlers = {};
 
-    this.subscribe = function (event, handler)
-    {
+    this.subscribe = function (event, handler) {
 
-        console.log(' * ARG: this.subscribe', arguments);
+      //  console.log(' * ARG: this.subscribe', arguments);
 
         if (handlers[event] === undefined)
         {
             handlers[event] = []; handlers[event].push(handler)
         }
+        console.log('hadlers:', handlers)
+
     };
 
-    this.publish = function (event)
-    {
-        console.log(' * ARG: this.publish', arguments);
+    this.publish = function (event) {
+      //  console.log(' * ARG: this.publish', arguments);
 
         if (handlers[event] === undefined) return;
 
-        var i = 0,
-            len = handlers[event].length;
+        var i = 0, len = handlers[event].length;
 
         for (i; i < len; i++)
         {
             handlers[event][i](arguments[i+1]);
         }
     };
+
 };
 
 pubSub = new PubSub();
 
-pubSub.subscribe('myEvent', function(arg){ console.log("myEvent worked. Arg: " + arg);});
+pubSub.subscribe('myEvent', function(arg){
+    console.log("myEvent worked. Arg: " + arg);
+});
 pubSub.publish('myEvent', 'it myArg');
 
-pubSub.subscribe('myNewEvent', function(arg){
-
-    console.log("myEvent worked. Arg: " + arg);});
+pubSub.subscribe('myNewEvent', function(arg2){
+    console.log('myNewEvent worked. Arg: ' + arg2 + ' plus new argumt');
+});
+pubSub.publish('myNewEvent', 'and... it myNEwArg');
+pubSub.publish('myNewEvent', 'and... it myNEwArg again');
