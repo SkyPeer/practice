656a534bd24f5a891ed01a458a55d3193209fe51
Oleg <o.selivancev@gmail.com>
2018-05-27 16:26:39 +0300

    this updated

--- Week5gh/this.js
+++ Week5gh/this.js
@@ -1,39 +1,41 @@
+// простой вызов функции
+/*
 function f() {
     console.log('THIS:',this === global); // true
 }
-f(); // простой вызов функции
+f();
+*/
 
+console.log('------------------------------------');
 
 //самовызывающаяся функция
-(function () {
+/*(function () {
     console.log('THIS:',this === global); // true
-})();
-
-
-
-
-
+})(); */
+console.log('------------------------------------');
 
 //вызов с конструктором
 //При вызове функции с использованием ключевого слова new функция выступает в роли конструктора, и в данном случе this указывает на создаваемый объект.
-function f() {
-    this.x = 5;
-    console.log('THIS:',this === global); // false
-    console.log('this a:', this.a);
+function f1() {
+    this.x1 = 255;
+    console.log('THIS ==== global :',this === global);
+    console.log('this a1:', this.a1);
+    console.log('this x1:', this.x1);
+   // return this;
 }
-
 
 // вызываем как метод объекта, this указывает на сам этот объект
 var o0 = {
-    a: 5,
-    yo: f
+    x1: 105,
+    yo: f1,
+    a1: 1024
 }
-console.log('o0.you() : ', o0.yo())
-// ----------
-var o = new f();
-console.log('o.x :',o.x); // 5
+//console.log('o0.you() : ', o0.yo())
 
+//console.log('f1',f1())
+// --------------------------------
 
+var o1 = new f1();  console.log('o1.x1 :',o1.x1, o1.a1);
 
 
 
