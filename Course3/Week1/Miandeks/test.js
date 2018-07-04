6644418e1a68967b6ed22227b017125c4366a701
Oleg <o.selivancev@gmail.com>
2018-07-04 05:10:44 +0300

    update week1 course 3
    grid switch worked

---
+++ Course3/Week1/Miandeks/test.js
@@ -0,0 +1,18 @@
+console.log("ла" < "ё")
+
+var a = 1;
+fun(a);
+
+var array = new Array();
+
+array.push('aaa')
+
+var b="ToToWe"
+
+console.log(b.slice(-2))
+
+function  fun() {
+    return 1+2;
+}
+console.log(fun())
+
