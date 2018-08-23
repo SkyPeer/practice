14c482c7c25a9f47d8b1d91c50b1a345cb8c76f9
Oleg <o.selivancev@gmail.com>
2018-08-23 11:29:02 +0300

    week1 prototype practice
    week1 prototype practice

--- Course4/Week1/proto.js
+++ Course4/Week1/proto.js
@@ -1,3 +1,4 @@
+/**
 var student = {
     type: 'robot',
     name: 'Billy',
@@ -24,13 +25,64 @@ console.log(student.age); //27
 console.log(person.age); //27
 console.log("-----------------");
 console.log(student.raiting);
+*/
 
 console.log("-----------------");
-var student = {};
-Object.prototype.serialize = function () {};
 
-console.log('serialize' in student);
+var student2 = {};
+Object.prototype.serialize = function () {};
+Array.prototype.serialize2 = function () {};
+var student2new = {};
+
+console.log('serialize' in student2);
 console.log('serialize' in []);
 console.log('serialize' in new Date());
 console.log('serialize' in Object);
 console.log('serialize' in Object.prototype);
+console.log('serialize' in student2new);
+console.log('serialize2' in Array.prototype);
+console.log("-----------------");
+
+
+console.log(Object.prototype.toString()); // [object Object]
+var student3 = {name: 'agent007'};
+var teacher3 = {name: 'TeacherOfAgent007'};
+/* student3.toString = function () {return 'NOOOOOOOOOOOO'}; */
+/* teacher3.toString = function () {return this.name}; */
+teacher3.toString = function () {return 'UHAHAHAH'};
+/*
+for (var keys1 in student3){console.log('student3-keys:');console.log(keys1)} //[object Object]
+for (var keys2 in teacher3){console.log('teacher3-keys:');console.log(keys2)}  //toString
+*/
+console.log(student3.toString()); //[object Object]
+console.log(teacher3.toString()); // ' UHAHAHAH '
+console.log(teacher3.toString.call(student3)); // ' UHAHAHAH '
+
+/*
+console.log("-----------------------------------------------");
+var student4 = {
+    name: 'Billy'
+};
+var person4 = {
+    getName: function () {
+        return this.name;
+    }
+};
+console.log(person4.getName.call(student4));
+console.log("--------------------------------------------------");
+    */
+console.log("--------------------------------------------------");
+var student5 = {};
+var person5 = {name: 'John', age:25};
+Object.setPrototypeOf(student5, person5);
+student5.name = "Mike";
+student5.lastname = "Dow";
+console.log(student5);
+console.log(person5);
+
+/*for (var student5keys in student5) {console.log(student5keys)}
+console.log('  ');
+for (var person5keys in person5) {console.log(person5keys)} */
+
+console.log('  ');
+console.log(student5.age);
