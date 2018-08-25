d93d7711f67cff2bc2351047a8af75471e0a13df
Oleg <o.selivancev@gmail.com>
2018-08-26 01:27:29 +0300

    week2 update

--- Course4/Week2/constructor2.js
+++ Course4/Week2/constructor2.js
@@ -1,15 +1,33 @@
-function StudentConstructor (name, lastname) {
+function StudentConstructor (name, lastname, familyname) {
     //this: StudentConstructor {}
+    var a = 25;
     this.name = name;
     this.lastname = lastname;
-    //console.log(this);
+    this.familyname = familyname;
+    /*console.log(this); */
     //this: StudentConstructor { name: 'ivan', lastname: 'ivanov' }
     //this указывает на вновь создаваемый объект
-    return {name: 'NewStudentName: ' + this.name, lastname: this.lastname}
+    //return {name: 'NewStudentName: ' + this.name, lastname: this.lastname, familyname: familyname};
 }
 
-var ivan = new StudentConstructor('ivan', 'ivanov');
+StudentConstructor.prototype =  {
+    sleepFunc: function (){
+        console.log('zzzzzZZZZZZZZZ....')
+    },
+     constructor: StudentConstructor
+};
+
+/* StudentConstructor.prototype.sleepFunc = function (){console.log('zzzzzZZZZZZZZZ....')}; */
+
+
+var ivan = new StudentConstructor('ivan', 'ivanov', 'ivanich');
 
 //ivan.constructor.name: Studentconstructor
 // Object.getPrototypeOf(ivan): StudentConstructor {}
-//ivan: { name: 'NewStudentName: ivan', lastname: 'ivanov' }
+//ivan: { name: ': ivan', lastname: 'ivanov',familyname: 'ivanich' }
+//ivan.sleepFunc(); zzzzzZZZZZZZZZ....
+
+console.log(StudentConstructor.prototype.constructor.name === 'StudentConstructor'); /* true */
+
+
+/* for (var keys in StudentConstructor.prototype){console.log(keys)} */  // sleepFunc constructor
