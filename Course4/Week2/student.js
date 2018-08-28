94e211150fefbbf6334bf4dabb0c023d35d8ccbc
Oleg <o.selivancev@gmail.com>
2018-08-28 18:57:35 +0300

    update

--- Course4/Week2/student.js
+++ Course4/Week2/student.js
@@ -2,7 +2,6 @@
 
 function Students(){
     this.students = [];
-
 }
 
 Students.from = function (arg) {
@@ -11,25 +10,41 @@ Students.from = function (arg) {
     return this;
 };
 
+Students.prototype.count = function ()
+{
+  return this.students.length;
+};
+
+
+Students.prototype.at = function(atArg) {
+    if (atArg > this.students.length || atArg <= 0 )
+    {
+        return null;
+    }
+    else
+    {
+        return this.students[--atArg];
+    }
+
+};
 Students.prototype.add = function (newstudent) {
 
     //console.log('Array.isArray:  ', Array.isArray(newstudent));
 
-    if (newstudent instanceof  Students){
+    if (newstudent instanceof  Students)
+    {
        console.log('это Students');
         for (var i=0; i<newstudent.students.length; i++)
         {
             this.students.push(newstudent.students[i])
         }
     }
-    else
-    {
+        else {
      //   console.log('это НЕ массив');
-        this.students.push(newstudent);
-    }
-
-
+            this.students.push(newstudent);
+        }
 };
+
 Students.prototype.all = function () {
     return this.students;
 };
@@ -38,10 +53,13 @@ Students.prototype.all = function () {
 a = new Students();
 a.add('ivan');
 a.add('peter');
-console.log('*A*  instance:',a instanceof Students,'| a.all: ',a.all());
+console.log('*A*  instance:',a instanceof Students,'| a.all: ',a.all(), '* COUNT :', a.count());
+
 b = Students.from([3,2]);
-console.log('*B*  instance:',b instanceof Students,'| b.all: ',b.all());
+console.log('*B*  instance:',b instanceof Students,'| b.all: ',b.all(), '* COUNT :', b.count());
+
 c = new Students();
 c.add(a);
 c.add(b);
-console.log('*C*  instance:',c instanceof Students,'| c.all: ',c.all());
+console.log('*C*  instance:',c instanceof Students,'| c.all: ',c.all(), '* COUNT :', c.count());
+console.log(c.at(5));
