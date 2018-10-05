5801fd748ea1fdb830d4c1cc7a028b6bee2ea2d8
Oleg <o.selivancev@gmail.com>
2018-10-06 01:13:18 +0300

    practice update

---
+++ Parctice/liter.js
@@ -0,0 +1,13 @@
+let a = true;
+let b = false;
+
+
+console.log( 'a=',(a ? 'TRUE!' : 'FALSE!'), ', b=', ( b ? 'TRUE!' : 'FALSE!') ) //a= ok , b= bad
+
+
+//------------------------------------------------------
+
+let a_result = a ? 'TRUE!' : 'FALSE!'
+let b_result = b ? 'TRUE!' : 'FALSE!'
+
+console.log('a=',a_result, ', b=',b_result)
