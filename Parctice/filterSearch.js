85fd01bb75f8c8d723a654962b9d8e0d771131bf
Oleg <o.selivancev@gmail.com>
2018-11-28 01:25:30 +0300

    ffiltersearch update

--- Parctice/filterSearch.js
+++ Parctice/filterSearch.js
@@ -1,4 +1,4 @@
-var phones = [
+let phones = [
     {title:'iPhone 7', company:'Apple'},
     {title:'iPhone 6S', company:'Apple'},
     {title:'Galaxy S8', company:'Samsung'},
@@ -28,28 +28,20 @@ console.log(filteredList())*/
 console.log(phones.match('sam'))*/
 console.log('-------------------------------------------')
 
-var searchArg='a'
+var searchArg='al'
 var pattern = new RegExp(searchArg, 'i')
 console.log('searhArg = ',searchArg)
-for (var i=0; i<phones.length; i++){
-    if (phones[i].company.match(pattern) /*  /sa/i  */){
-        console.log(phones[i], )
-    }
-}
+
+    /*for (var i=0; i<phones.length; i++){
+        if (phones[i].title.match(pattern)  || phones[i].company.match(pattern) ){
+            console.log(phones[i], )
+        }
+    } */
+
+let filteredArray = phones.filter(item => {
+    return item.title.match(pattern) || item.company.match(pattern)
+})
+
+console.log(filteredArray)
 
 console.log('-------------------------------------------')
-
-var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lest. Donec convallis dignissim ligula, et rutrum est elat vistibulum eu.';
-
-// Подойдут оба и "elit", и "elat". Точка означает, что подойдет любой символ.
-
-var regex = /e./g;
-
-console.log( text.match(regex) );
-
-
-// "est" и "lest" одинаково подойдут. Знак вопроса делает "l" необязательной.
-
-var regex2 = /l?est/g;
-
-console.log( text.match(regex2) );
