991ebb2e21f1fd9cae5e783cedea080c1deedae6
Oleg <o.selivancev@gmail.com>
2019-01-20 01:06:10 +0300

    practive update

--- Parctice/forEach.js
+++ Parctice/forEach.js
@@ -2,7 +2,7 @@ var a = [1,10,2,8,9]
 var b = []
 
     a.forEach(function (item, i, arr) {
-        console.log('item = ',item,' i = ',i, 'array = ',arr)
+        console.log('item = ',item,' i = ',i, ' array = ',arr)
         if (arr[i] > 3) {
             b.push(arr[i])
         }
