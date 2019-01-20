d2e0369f0d561835700c1c5b4a858fd590c4a27b
Oleg <o.selivancev@gmail.com>
2019-01-21 01:36:33 +0300

    practice update

--- Parctice/splice.js
+++ Parctice/splice.js
@@ -8,9 +8,9 @@ if (index > -1) {
 console.log(array)
 
 
-var animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+var animals = ['ant', 'bison', 'camel', 'donaldduck', 'elephant'];
 
 console.log("count: ",animals.length)
 animals.splice(0,1);
 console.log(animals)
-// expected output: Array ["camel", "duck", "elephant"]
+// expected output: Array ["camel", "donaldduck", "elephant"]
