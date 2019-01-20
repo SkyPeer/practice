cde698b2f54d63c14f9b18f3ff49ce2a799c79d4
Oleg <o.selivancev@gmail.com>
2019-01-21 01:41:26 +0300

    nodejs starting script update

--- Parctice/NodeJS/test-start.js
+++ Parctice/NodeJS/test-start.js
@@ -1,4 +1,8 @@
 
+// https://nodejs.org/docs/latest/api/process.html
+// $ node process-args.js one two=three four
+// cmd: node test-start arg=123
+
 //node test-start.js user passs
 console.log('NodeJs StartTestingScript')
 
