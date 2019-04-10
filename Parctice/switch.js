bbe1429b89734641874be31b055511ef8ecf2c64
Oleg <o.selivancev@gmail.com>
2019-04-11 01:29:15 +0300

    Update switch.js

--- Parctice/switch.js
+++ Parctice/switch.js
@@ -1,13 +1,16 @@
 
 function hello(arg) {
     let a = arg
-
+    let b = arg - 15
     switch (true) {
         case(a < 5)  :
             console.log('1')
             break
         case(a < 10)  :
             console.log('2')
+            break;
+        case(b == 0):
+            console.log('---ba case')
             break;
         default:
             console.log('i feel good')
