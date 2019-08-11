fc76b38a6e7198564abe6d0ecce8b3c44b46cb69
Oleg Selivantsev <o.selivancev@gmail.com>
2019-08-11 23:23:52 +0300

    Update this.js

--- Parctice/2019/this.js
+++ Parctice/2019/this.js
@@ -1,14 +1,12 @@
-var firstName = 'Tom';
-var lastName = 'Smit';
+// var firstName = 'JohnDefault';
+// var lastName = 'SmithDefault';
 
 var user = {
     id : 5,
     age: 33,
     firstName: 'Tom',
-    lastName: 'Smit',
-    /*getFullName: () => {
-        return (this.firstName +' '+ this.lastName);
-    }*/
+    lastName: 'Smith',
+    
     getFullName: function () {
         return (this.firstName + ' '+ this.lastName)
     }
@@ -19,12 +17,39 @@ var user2 = {
     lastName: 'Johns'
 };
 
-console.log('/user.name:', user.getFullName()); //user.name: Tom Smit
-console.log('/user.name:', user.getFullName.apply(user2)); //user.name: Alex Johns
-console.log('----------------------------------------------------------');
 
-function ahhh(){
-    console.log(this)
+// console.log('/user.name:', user.getFullName()); //user.name: Tom Smit
+// console.log('/user.name:', user.getFullName.apply(user2)); //user.name: Alex Johns
+// console.log('----------------------------------------------------------');
+
+function fuu(value, value2, value3, param){
+    console.log(this.firstName, ' / ', this.lastName, 'Value:', [value], 'Value2:', value2, 'value3', value3, `this[param] ${param}:`, this[param])
+    //console.log(this[param])
 }
 
-ahhh.apply(user2); //{ firstName: 'Alex', lastName: 'Johns' }
+//fuu.bind(user); //{ firstName: 'Tom', lastName: 'Smit' }
+
+console.log(' - - - - - - - - - - - - - - - - ')
+// fuu.call(user, 'test_1', 'test_2', 'test_3', 'age'); //{ firstName: 'Top', lastName: 'Smit' }
+
+console.log(' - - - - - - - - - - - - - - - - ');
+// fuu.apply(user, ['test1', 'test2', 'sasdsa', 'age'])
+
+// - - - - - - - - BIND - - - - - 
+var getFullname = user.getFullName;
+var aaa = getFullname.bind(user)
+console.log('aaa', aaa())
+// - - - - - - - - - - - - - - - 
+
+
+function Person(user) {
+    this.firstName = user.firstName
+    this.lastName = user.lastName
+}
+
+Person.prototype = user
+
+var person = new Person(user2)
+//console.log('person', person.firstName, 'lastName', person.lastName, 'age', person.age)
+console.log('PersonObj:', person, 'PersonAgeProto', person.age)
+//console.log('userObj', user)
