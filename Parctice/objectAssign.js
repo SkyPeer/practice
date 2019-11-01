11771fbb7bddda23b07703d7ae2b58f93155d5bf
Oleg Selivantsev <o.selivancev@gmail.com>
2019-11-01 04:32:01 +0300

    Create objectAssign.js

---
+++ Parctice/objectAssign.js
@@ -0,0 +1,21 @@
+var users = [
+  { 120: [{ user: "ivan", pass: "123" }, { user: "peter", pass: "foo" }] },
+  { 130: [{ user: "john", pass: "33sa" }, { user: "jack", pass: "zzz" }] },
+  {
+    100: [{ user: "sara", pass: "bbb" }, { user: "sierra", pass: "rogger" }]
+  }
+];
+var groups = [{ 120: "Users" }, { 100: "Admins" }, { 130: "Rootes" }];
+
+var usersWithGroups = users.map(user => {
+  var userHaveGroupFinder = group => {
+    return Object.keys(user)[0] == Object.keys(group)[0];
+  };
+
+  var userWithEqualGroup = groups.find(userHaveGroupFinder);
+  var gorupOjb = userWithEqualGroup;
+  console.log("userWithEqualGroup", user, "-:-", gorupOjb);
+
+  return Object.assign({}, user, { group: Object.values(gorupOjb)[0] });
+});
+console.log("usersWithGroups", usersWithGroups[1]);
