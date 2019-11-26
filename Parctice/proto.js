8dc60a3b1895d8227d77e5533368a6af9b2fda7b
Oleg Selivantsev <o.selivancev@gmail.com>
2019-11-27 00:38:48 +0300

    Update proto.js

--- Parctice/proto.js
+++ Parctice/proto.js
@@ -1,12 +1,12 @@
 function person(first, name) {
-    this.first = first;
-    this.name = name
+  this.first = first;
+  this.name = name;
 }
 
-person.prototype.getname = function (mr) {
-    return (mr + ' ' + this.name + ' ' + this.first)
-}
+person.prototype.getname = function(mr) {
+  return mr + " " + this.name + " " + this.first;
+};
 
-var myPerson = new person('oleg', 'petrov')
+var myPerson = new person("oleg", "petrov");
 
-console.log(myPerson.getname('mr'))
+console.log(myPerson.getname("111"));
