fcb77801b91538cc4fa3c6e3daca5e645a00004e
Oleg Selivantsev <o.selivancev@gmail.com>
2019-12-09 23:54:08 +0300

    Update func.js

--- Parctice/func.js
+++ Parctice/func.js
@@ -1,6 +1,16 @@
-let test = { a: {name:'Вася'}/*, toString(){return 'Наташа'}, valueOf(){return this.a}*/}
+let test = {
+  a: {
+    name: "Вася"
+  } /*, toString(){return 'Наташа'}, valueOf(){return this.a}*/
+};
 
 console.log(test);
-test.a.name = 'Петя'
-console.log(test)
+test.a.name = "Петя";
+console.log(test);
 
+function foo() {
+  return function f(e) {
+    console.log("f", e);
+  };
+}
+foo()(2)(4);
