d3d4004c910a61220f52e139483dee46e14016c0
Oleg Selivantsev <o.selivancev@gmail.com>
2025-04-13 04:57:30 +0400

    update

--- Python/addTwoNumbers.py
+++ Python/addTwoNumbers.py
@@ -1,63 +1,35 @@
-class ListNode:
-    def __init__(self, val=0, carry=None):
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.carry = carry
+        self.next = next
 
-def foo(list1, list2):
-    idx = 0
-    length1 = len(list1)
-    length2 = len(list2)
-    max_length = max(length1, length2)
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        print(l1)
+        dummy = ListNode()
+        current = dummy
+        carry = 0
 
-    # print('max_length', max_length)
-
-    result_list = []
-
-    while idx < max_length:
-
-        try:
-            l1_value = list1[idx]
-        except:
+        while l1 or l2 or carry:
+            sum_val = carry
             l1_value = 0
-
-        try:
-            l2_value = list2[idx]
-        except:
             l2_value = 0
 
+            if l1:
+                l1_value = l1.val
+                l1 = l1.next
 
-        sum = l1_value + l2_value
+            if l2:
+                l2_value = l2.val
+                l2 = l2.next
 
-        if idx:
-            carry = result_list[idx - 1].carry
-            sum += carry
+            result = divmod((l1_value + l2_value) + sum_val, 10)
+            carry = result[0]
+            val = result[1]
 
-        result = divmod(sum, 10)
+            current.next = ListNode(val)
+            current = current.next
 
-        carry = result[0]
-        val = result[1]
-
-        # print('-------', idx, '-------')
-        # print('val', val)
-        # print('carry', carry)
-        # print('--------------')
-        # node =
-        result_list.append(ListNode(val, carry))
-        idx += 1
-
-    if result_list[max_length -1].carry:
-        result_list.append(ListNode(result_list[max_length -1].carry, 0))
-
-
-    print('--------------')
-    print('Result')
-    i=0
-    while i < len(result_list):
-        print(result_list[i].val, result_list[i].carry)
-        i += 1
-    print('--------------')
-
-
-foo([2,4,3], [5,6,4]) #[7,0,8]
-foo([0], [0])
-foo([9,9,9,9,9,9,9], [9,9,9,9])
+        print(dummy.next)
+        return dummy.next
