967cfd39029f54af56f880057b24c9ffad53748b
Oleg Selivantsev <o.selivancev@gmail.com>
2025-04-19 00:08:16 +0400

    Update TwoSum.py

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/two-sum/
+# Two Sum
+
 # https://www.w3schools.com/python/python_sets.asp
 # a = ["Geeks", "for", "Geeks"]
 # # Iterating list using enumerate to get both index and element
@@ -6,8 +9,6 @@
 #
 # # Converting to a list of tuples
 # print(list(enumerate(a)))
-
-# https://leetcode.com/problems/two-sum/
 
 # hashMap = {}
 # for e in list1:
@@ -28,17 +29,22 @@ class Solution:
 
         # convert nums to dict
         nums_dict = {value: index for index, value in enumerate(nums)}
-        print('nums_dict:', nums_dict)
 
         result_set = set()
         for idx, value in enumerate(nums):
             dict_value = nums_dict.get(target - value)
-            if dict_value:
+            if dict_value and idx != dict_value:
                 result_set.add(idx)
                 result_set.add(dict_value)
 
         return list(result_set)
 
-print(Solution.twoSum([2,7,11,15], 9)) # [0,1]
-print(Solution.twoSum([3,2,4], 6)) # [1,2]
-print(Solution.twoSum([3,3], 6)) # [0,1]
+
+
+# print(Solution.twoSum([2,7,11,15], 9)) # [0,1]
+# print(Solution.twoSum([3,2,4], 6)) # [1,2]
+# print(Solution.twoSum([3,3], 6)) # [0,1]
+# print(Solution.twoSum([-3,4,3,90], 0)) # [0,2]
+# print(Solution.twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 11)) # [5,11]
+# print(Solution.twoSum([0,3,-3,4,-1], -1)) # [0,4]
+print(Solution.twoSum([-1,-2,-3,-4,-5], -8)) # [2,4]
