8bdfebb3fb727bc1cef03c9fe114cf1ba8c0d606
Oleg Selivantsev <o.selivancev@gmail.com>
2025-04-26 01:52:37 +0400

    debug

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -30,6 +30,8 @@ class Solution:
         # convert nums to dict
         nums_dict = {value: index for index, value in enumerate(nums)}
 
+        print(nums_dict)
+
         result_set = set()
         for idx, value in enumerate(nums):
             dict_value = nums_dict.get(target - value)
@@ -38,13 +40,13 @@ class Solution:
                 result_set.add(dict_value)
 
         return list(result_set)
+    pass
 
 
-
-# print(Solution.twoSum([2,7,11,15], 9)) # [0,1]
+print(Solution.twoSum([2,7,11,15], 9)) # [0,1]
 # print(Solution.twoSum([3,2,4], 6)) # [1,2]
 # print(Solution.twoSum([3,3], 6)) # [0,1]
 # print(Solution.twoSum([-3,4,3,90], 0)) # [0,2]
 # print(Solution.twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 11)) # [5,11]
 # print(Solution.twoSum([0,3,-3,4,-1], -1)) # [0,4]
-print(Solution.twoSum([-1,-2,-3,-4,-5], -8)) # [2,4]
+# print(Solution.twoSum([-1,-2,-3,-4,-5], -8)) # [2,4]
